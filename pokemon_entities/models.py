48b6851894becf9acb535605f39722c9d5d30895
Бирюков Максим <maxandix@gmail.com>
2020-04-17 11:57:41 +0300

    add models Pokemon, PokemonEntity

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# your models here
+
+class Pokemon(models.Model):
+    title = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='pokemon', null=True, blank=True)
+
+    def __str__(self):
+        return f'Pokemon "{self.title}"'
+
+class PokemonEntity(models.Model):
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    lat = models.FloatField()
+    lon = models.FloatField()
+    appeared_at = models.DateTimeField()
+    disappeared_at = models.DateTimeField()
+
+    level = models.IntegerField()
+    health = models.IntegerField()
+    strength = models.IntegerField()
+    defence = models.IntegerField()
+    stamina = models.IntegerField()
+
+# def __str__(self):
+    #     return f'Pokemon "{self.title}"'
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -3,7 +3,8 @@ import json
 
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
-
+from .models import PokemonEntity, Pokemon
+from django.core.exceptions import ObjectDoesNotExist
 
 MOSCOW_CENTER = [55.751244, 37.618423]
 DEFAULT_IMAGE_URL = "https://vignette.wikia.nocookie.net/pokemon/images/6/6e/%21.png/revision/latest/fixed-aspect-ratio-down/width/240/height/240?cb=20130525215832&fill=transparent"
@@ -22,22 +23,28 @@ def add_pokemon(folium_map, lat, lon, name, image_url=DEFAULT_IMAGE_URL):
 
 
 def show_all_pokemons(request):
-    with open("pokemon_entities/pokemons.json", encoding="utf-8") as database:
-        pokemons = json.load(database)['pokemons']
+    # with open("pokemon_entities/pokemons.json", encoding="utf-8") as database:
+    #     pokemons = json.load(database)['pokemons']
+    pokemons = Pokemon.objects.all()
+
+    # print(type(pokemons.pokemonentity_set))
+    # print(dir(pokemons.pokemonentity_set[0]))
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     for pokemon in pokemons:
-        for pokemon_entity in pokemon['entities']:
+        print(PokemonEntity.objects.filter(pokemon=pokemon).first())
+        for pokemon_entity in PokemonEntity.objects.filter(pokemon=pokemon):
+            print(pokemon.title, pokemon.photo.url)
             add_pokemon(
-                folium_map, pokemon_entity['lat'], pokemon_entity['lon'],
-                pokemon['title_ru'], pokemon['img_url'])
+                folium_map, pokemon_entity.lat, pokemon_entity.lon,
+                pokemon.title, request.build_absolute_uri(pokemon.photo.url))
 
     pokemons_on_page = []
     for pokemon in pokemons:
         pokemons_on_page.append({
-            'pokemon_id': pokemon['pokemon_id'],
-            'img_url': pokemon['img_url'],
-            'title_ru': pokemon['title_ru'],
+            'pokemon_id': pokemon.id,
+            'img_url': request.build_absolute_uri(pokemon.photo.url),
+            'title_ru': pokemon.title,
         })
 
     return render(request, "mainpage.html", context={
@@ -57,11 +64,18 @@ def show_pokemon(request, pokemon_id):
     else:
         return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
 
+    try:
+        pokemon = Pokemon.objects.get(id=int(pokemon_id))
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
+
+    pokemon_entitys = PokemonEntity.objects.filter(pokemon=pokemon)
+
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon_entity in requested_pokemon['entities']:
+    for pokemon_entity in pokemon_entitys:
         add_pokemon(
-            folium_map, pokemon_entity['lat'], pokemon_entity['lon'],
-            pokemon['title_ru'], pokemon['img_url'])
+            folium_map, pokemon_entity.lat, pokemon_entity.lon,
+            pokemon.title, request.build_absolute_uri(pokemon.photo.url))
 
     return render(request, "pokemon.html", context={'map': folium_map._repr_html_(),
                                                     'pokemon': pokemon})
